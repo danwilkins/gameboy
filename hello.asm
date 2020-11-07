538bd025082ebaa0e5c36d60d3d28cf2a3e5c8a4
Brent Farris <brentfarris@outlook.com>
2020-11-07 01:59:14 -0600

    Updating to have input printing

--- hello.asm
+++ hello.asm
@@ -1,5 +1,15 @@
 INCLUDE "hardware.inc"
 
+CURSOR		EQU $04
+JOY_LEFT	EQU $20
+JOY_UP		EQU $40
+JOY_RIGHT	EQU $10
+JOY_DOWN	EQU $80
+JOY_A		EQU $01
+JOY_B		EQU $02
+JOY_START	EQU $08
+JOY_SELECT	EQU $04
+
 ; Helpful section name
 SECTION "Header", ROM0[$100]	; Program starts at $100
 EntryPoint: ; This is where execution begins
@@ -12,8 +22,9 @@ REPT $150 - $104
 ENDR
 
 SECTION "V Blank", ROM0[$0040]	; The address for V-Blank interrupt routine
-inc a	; Increment the scroll
-reti	; Return and reset the interrupt flag
+ld a, $00
+;inc a		; Increment the scroll
+reti		; Return and reset the interrupt flag
 
 SECTION "Game code", ROM0	; No need to declare where to put this code, let assembler pick
 Start:
@@ -39,18 +50,29 @@ ld bc, FontTilesEnd - FontTiles	; Length of font tiles
 	or c				; a OR c (should be 0 if both are 0)
 	jr nz, .copyFont	; Loop if not done yet
 
-ld hl, $9800			; This will print the string at the top-left corner of the screen
-ld de, HelloWorldStr	; Load the address of the HelloWorldStr data into de
+ld hl, _SCRN0+$20		; This will print the string at the top-left corner of the screen
+ld bc, $1A
+ld de, SudokuBoard
+.copyBoard
+	ld a, [de]
+	inc de
+	cp a, $FE
+	jr z, .nextLine
+	cp a, $FF
+	jr z, .bgSet
+	ld [hli], a
+	jr .copyBoard
+.nextLine
+	add hl, bc
+	jr .copyBoard
 
-.copyString
-	ld a, [de]			; Load value at memory pointed to by de pair into a
-	ld [hli], a			; Load a into the memory pointed to by hl pair and increment hl
-	inc de				; Decrement the address of de
-	and a				; Check if the byte we just copied is zero '\0'
-	jr nz, .copyString	; Loop if not null string terminator ('\0')
+.bgSet
+	ld a, %11100100		; Pick a background color
+	ld [rBGP], a		; Set the background color
 
-ld a, %11100100			; Pick a background color
-ld [rBGP], a			; Set the background color
+.drawCursor
+	ld a, $04
+	ld [_SCRN0], a		; Cursor value is 4
 
 xor a					; Zero out a register
 ld [rSCY], a			; Set scroll y to 0
@@ -60,15 +82,116 @@ ld [rNR52], a			; Turn off sound (a is still 0)
 ld a, %10000001			; Bit 7 = screen on, bit 0 = background on
 ld [rLCDC], a			; Set LCD flags
 
+xor a
 .lockup
-	ld [rSCX], a		; Set scroll x
+	cp a, $00
+	jr nz, .lockup
+	di
+	call .joy_con
+	call .read
+	xor a
+	ei
+	ld a, %00000001		; We only want the v-blank interrupt
+	ld [rIE], a			; Set the interrupts flags
 jr .lockup				; Game lockup loop
+
+.joy_con
+	ld a, $20
+	ld [rP1], a
+	ld a, [rP1]
+	ld a, [rP1]		; Wait a few cycles
+	cpl 			; Complement a
+	and $0f			; Only get first 4 bits
+	swap a
+	ld b, a
+	ld a, $10
+	ld [rP1], a
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	cpl
+	and $0f
+	or b
+	ret
+
+.read
+	ld b, a
+	xor a					; Set a to 0
+	ld [rLCDC], a			; Turn off the LCD by writing 0
+	ld a, b
+	ld hl, _SCRN0
+	; Going to do this the dumb way for testing
+		ld de, Inputs
+		and JOY_LEFT
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_UP
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_RIGHT
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_DOWN
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_A
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_B
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_START
+		call z, .erase_sprite
+		call nz, .write_sprite
+		inc de
+		ld a, b
+		and JOY_SELECT
+		call z, .erase_sprite
+		call nz, .write_sprite
+	; End of dumb
+	ld a, %10000001			; Bit 7 = screen on, bit 0 = background on
+	ld [rLCDC], a			; Set LCD flags
+	ret
+
+.erase_sprite
+	ld a, $00
+	ld [hli], a
+	ret
+.write_sprite
+	ld a, [de]
+	ld [hli], a
+	ret
 
 SECTION "Font", ROM0	; No need to declare where to put this code, let assembler pick
 FontTiles:
 INCBIN "font.chr"		; Copy contents of file to right here
 FontTilesEnd:
 
-SECTION "Hello World string", ROM0	; No need to declare where to put this code, let assembler pick
-HelloWorldStr:
-    db "Hello World!", 0	; Define byte (ascii string) 0 is string null terminator
+SECTION "Strings", ROM0	; No need to declare where to put this code, let assembler pick
+Inputs:
+    db "LURDABSE"		; Define byte (ascii string) 0 is string null terminator
+
+SECTION "Sudoku Board", rom0
+SudokuBoard:
+	; $00 = blank, $01 = cross, $02 = vertical, $03 = horizontal
+	db $00, $02, $00, $02, $00, $FE,
+	db $03, $01, $03, $01, $03, $FE,
+	db $00, $02, $00, $02, $00, $FE,
+	db $03, $01, $03, $01, $03, $FE,
+	db $00, $02, $00, $02, $00, $FE,
+	db $FF
